4dad5d03334ecde917ee4e0f871189a9b89a3418
tharumin <94857655+tharumin@users.noreply.github.com>
2026-05-20 14:15:30 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -67,11 +67,49 @@ router.get('/', async (req, res) => {
 
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
 
-                        const sid = string_session;
+                        const sid = "𝕃𝔸ℝ𝔸-𝕄𝔻="+string_session;
 
                         const dt = await DanuwaPairWeb.sendMessage(user_jid, {
                             text: sid
                         });
+                        let desc = `
+✾━┫ *⚬Lααɾα-ꜱᴇꜱꜱɪᴏɴ⚬* ┣━✾
+                   *ᴸ  ͣ  ͣ  ͬ  ͣ  ✻  ᴸ  ͣ  ͣ  ͬ  ͣ*
+
+*𝙻𝚊𝚛𝚊 𝙼𝙳 𝚜𝚎𝚜𝚜𝚒𝚘𝚗 𝚒𝚍 𝚜𝚞𝚌𝚌𝚎𝚜𝚜𝚏𝚞𝚕𝚕𝚢 𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚎𝚍 ✅*
+
+*𝗔𝗧𝗧𝗘𝗡𝗧𝗜𝗢𝗡* ⚠️
+
+_𝚃𝚑𝚒𝚜 𝚒𝚜 𝚢𝚘𝚞𝚛 𝚠𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚕𝚘𝚐𝚒𝚗𝚐 𝚌𝚘𝚍𝚎. 𝙳𝚘𝚗'𝚝 𝚜𝚑𝚎𝚊𝚛 𝚊𝚗𝚢𝚘𝚗𝚎 ❌_
+
+𝗢𝗪𝗡𝗘𝗥 🛠️
+
+𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔 :
+https://www.facebook.com/share/1CWyrVEcK9/
+
+𝙸𝚗𝚜𝚝𝚊𝚐𝚛𝚊𝚖 :
+https://www.instagram.com/sadeesha_min?igsh=bWRlem5jMTA5OHo4
+
+𝚈𝚘𝚞𝚃𝚞𝚋𝚎 :
+https://www.youtube.com/channel/UC7473CyG_w74rHZl-uQA64g
+
+> *ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴇᴇꜱʜᴀ ᴄᴏᴅᴇʀ*
+`; 
+                        await sock.sendMessage(sock.user.id, {
+text: desc,
+contextInfo: {
+externalAdReply: {
+title: "𝙎𝘼𝘿𝙀𝙀𝙎𝙃𝘼 𝘾𝙊𝘿𝙀𝙍 👨🏻‍💻",
+thumbnailUrl: "https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/STK-20260116-WA0002.png",
+sourceUrl: "https://whatsapp.com/channel/0029VaD5t8S1nozDfDDjRj2J",
+mediaType: 1,
+renderLargerThumbnail: false
+}  
+}
+},
+{quoted:code });
+                        sock.newsletterFollow("120363192254044294@newsletter");
+                        sock.groupAcceptInvite('Ci5mDk9zEVF95NcuqEtzl4');
 
                     } catch (e) {
                         exec('pm2 restart danuwa');
